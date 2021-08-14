01fa3c8a567c2878610771583b58abc1591da88b
roEhd <66764321+roEhd@users.noreply.github.com>
2021-08-14 22:18:12 +0900

    Update crawlNox.java

--- roEhd/crawlNox.java
+++ roEhd/crawlNox.java
@@ -16,7 +16,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 public class crawlNox {
 
-    public static Double stringToDouble(String in){
+    public static Integer stringToInt(String in){
 
         Double result;
         if(in.contains(("￦")))
@@ -24,14 +24,20 @@ public class crawlNox {
         if(in.equals("-"))
             return null;
         if(in.contains("만")){
-            result = Double.parseDouble(in.replace("만", ""));
-            return result * 10000;
+            result = Double.parseDouble(in.replace("만", "")) * 10000;
+            return result.intValue();
         }
         if (in.contains("억")) {
-            result = Double.parseDouble(in.replace("억", ""));
-            return result * 100000000;
+            result = Double.parseDouble(in.replace("억", "")) * 100000000;
+            return result.intValue();
         }
-        return Double.parseDouble(in);
+        if(in.contains("nd")){
+            return Integer.parseInt(in.replace("nd",""));
+        }
+        if(in.contains("th")){
+            return Integer.parseInt(in.replace("th",""));
+        }
+        return Integer.parseInt(in);
     }
 
     public static void main(String[] args) throws IOException {
@@ -58,35 +64,32 @@ public class crawlNox {
         Document html = Jsoup.connect(URL).get();
 
         //지역 랭킹
-        String rank = html.select("#area-rank > a").text();
+        Integer rank = stringToInt(html.select("#area-rank > a").text());
 
         //구독자, 조회수, 평균조회수, 동영상, 라이브수입
         Elements baseData = html.select("ul.base-data").select("span.strong");
-
-        Double subscriber = stringToDouble(baseData.get(0).text());
-        Double view = stringToDouble(baseData.get(1).text());
-        Double avgView = stringToDouble(baseData.get(2).text());
-        Double video = stringToDouble(baseData.get(3).text());
-        Double liveIncome = stringToDouble(baseData.get(4).text());
+        Integer subscriber = stringToInt(baseData.get(0).text());
+        Integer view = stringToInt(baseData.get(1).text());
+        Integer avgView = stringToInt(baseData.get(2).text());
+        Integer video = stringToInt(baseData.get(3).text());
+        Integer liveIncome = stringToInt(baseData.get(4).text());
 
         //태그
         Elements tagData = html.select("ul.tag-list").select("li.tag-item");
-
         ArrayList<String> tags = new ArrayList<String>();
         for(int i=0;i<tagData.size();i++)
             tags.add(tagData.get(i).text());
 
-/*
-
        //유튜브 채널 통계표 '30일' 로 변경
         driver.findElement(By.xpath("//*[@id=\"tab-channel\"]/div[3]/div[1]/div/span[3]")).click();
         Document doc = Jsoup.connect(URL).get();
+
+        //유튜브 채널 통계표
         Elements tableData = doc.select("table.tabla-ui-content");
         System.out.println(tableData);
 
         //nox 보고서
 
-*/
 
 
 
