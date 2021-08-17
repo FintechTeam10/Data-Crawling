85b24f5cdcc0adcdc4a04015a493ba10a3324767
roEhd <66764321+roEhd@users.noreply.github.com>
2021-08-17 23:42:02 +0900

    Update crawlNox.java

--- roEhd/crawlNox.java
+++ roEhd/crawlNox.java
@@ -1,22 +1,30 @@
 package team10.moneyTube.service.crawl;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
-
+import java.util.Date;
+import lombok.Data;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.Connection;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+@Data
+class channelInformation{
+    private Date date;
+    private Long subscriber;
+    private Long changedSubscriber;
+    private Long view;
+    private Long changedView;
+}
+
 public class crawlNox {
 
-    public static Integer stringToInt(String in){
+    private static Long stringToInt(String in){
 
         Double result;
         if(in.contains(("￦")))
@@ -25,19 +33,19 @@ public class crawlNox {
             return null;
         if(in.contains("만")){
             result = Double.parseDouble(in.replace("만", "")) * 10000;
-            return result.intValue();
+            return result.longValue();
         }
         if (in.contains("억")) {
             result = Double.parseDouble(in.replace("억", "")) * 100000000;
-            return result.intValue();
+            return result.longValue();
         }
         if(in.contains("nd")){
-            return Integer.parseInt(in.replace("nd",""));
+            return Long.parseLong(in.replace("nd",""));
         }
         if(in.contains("th")){
-            return Integer.parseInt(in.replace("th",""));
+            return Long.parseLong(in.replace("th",""));
         }
-        return Integer.parseInt(in);
+        return Long.parseLong(in);
     }
 
     public static void main(String[] args) throws IOException {
@@ -64,15 +72,18 @@ public class crawlNox {
         Document html = Jsoup.connect(URL).get();
 
         //지역 랭킹
-        Integer rank = stringToInt(html.select("#area-rank > a").text());
+        Long rank = stringToInt(html.select("#area-rank > a").text());
 
         //구독자, 조회수, 평균조회수, 동영상, 라이브수입
         Elements baseData = html.select("ul.base-data").select("span.strong");
-        Integer subscriber = stringToInt(baseData.get(0).text());
-        Integer view = stringToInt(baseData.get(1).text());
-        Integer avgView = stringToInt(baseData.get(2).text());
-        Integer video = stringToInt(baseData.get(3).text());
-        Integer liveIncome = stringToInt(baseData.get(4).text());
+        Long subscriber = stringToInt(baseData.get(0).text());
+        Long view = stringToInt(baseData.get(1).text());
+        Long avgView = stringToInt(baseData.get(2).text());
+        Long video = stringToInt(baseData.get(3).text());
+        Long liveIncome = stringToInt(baseData.get(4).text());
+
+        //nox 보고서
+        String nox_score = html.select("div.noxscore-content span.score").text().strip();
 
         //태그
         Elements tagData = html.select("ul.tag-list").select("li.tag-item");
@@ -80,15 +91,16 @@ public class crawlNox {
         for(int i=0;i<tagData.size();i++)
             tags.add(tagData.get(i).text());
 
-       //유튜브 채널 통계표 '30일' 로 변경
-        driver.findElement(By.xpath("//*[@id=\"tab-channel\"]/div[3]/div[1]/div/span[3]")).click();
-        Document doc = Jsoup.connect(URL).get();
-
         //유튜브 채널 통계표
-        Elements tableData = doc.select("table.tabla-ui-content");
-        System.out.println(tableData);
+        Elements tableData = html.select("table.tabla-ui-content");
+        System.out.println(tableData.text());
+        ArrayList<channelInformation> channel = new ArrayList<>();
+        for (Element data : tableData) {
+            System.out.println(data.text());
 
-        //nox 보고서
+        }
+
+
 
 
 
